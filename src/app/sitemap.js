95691aac2579733ececa6a51296321a704aa4909
franklyn101git config --global user.email franklyninoma@gmail.comgit pull git config --global user.name franklyn101 <franklyninoma@gmail.com>
2025-12-04 21:23:57 +0100

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://latev-engineering.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+  ];
+}
